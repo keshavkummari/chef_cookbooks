85770d8ee74d04f4cf08dfb4f5963c5fdf67ab30
keshavkummari <keshav.kummari@gmail.com>
2019-01-12 05:32:36 +0000

    Apache

---
+++ apache/recipes/server.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+	content '<h1>Welcome to Chef World</h1>'
+end
+
+service 'httpd' do
+	action [ :enable, :start ]
+end
+
